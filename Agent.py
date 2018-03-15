83a527166de64418dda7371cfb173815d0035954
githubcyclist <mouse869@gmail.com>
2018-03-15 07:29:36 -0400

    Create Agent.py

---
+++ Agent.py
@@ -0,0 +1,10 @@
+# The Agent class will be
+# the base class of OpenAgent
+# for Python.
+class Agent:
+  # Say something.
+  def say(text):
+    
+  # Play an animation.
+  def animate(name):
+    
